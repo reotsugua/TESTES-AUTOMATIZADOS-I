eca8268ad6619df150d4ddae82a6fef45d960d94
reotsugua <renanaugusto.ras@gmail.com>
2023-05-05 20:25:25 -0300

    aula 01

--- Aula/src/test/java/br/com/service/PersonServiceTest.java
+++ Aula/src/test/java/br/com/service/PersonServiceTest.java
@@ -1,5 +1,16 @@
 package br.com.service;
 
+import br.com.domain.Person;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class PersonServiceTest {
+    @Test
+    public void checkAgeOver18(){
+        Person person = new Person(18);
+        PersonService service = new PersonService();
+        Boolean result = service.isAdult(person);
+        Assertions.assertTrue(result);
+    }
 
 }
